635619216914efd5a825d0cd26af59baa6b3d0e5
Vinay Dwivedi <vindwivedi@paypal.com>
2021-07-21 21:23:04 +0530

    leetcode 001 java

---
+++ java/001.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        Map <Integer, Integer> map = new HashMap();
+        int[] result = new int[2];
+        
+        for(int i=0; i < nums.length; i++  ){
+            
+            int diff = target - nums[i];
+            if(map.containsKey(diff)){
+                result[0] = map.get(diff);
+                result[1] = i;
+                return result;
+            }else{
+                map.put(nums[i], i);
+            }
+        }
+        
+        return result;
+    }
+}
